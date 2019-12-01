a03c94d4ecced32769236ee4353c149de44f337a
lauren diaz <lauren@laurens-MBP.hsd1.fl.comcast.net>
2019-12-01 15:05:11 -0500

    updated server.js to handle pug, fixed readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # filmHub
 
-a ***better*** way to do movies
+## what is filmHub?
+a **better** way to do movies
+
+## usage
+1. $ cd filmhub/src
+2. $ nodemon server.js
+3. navigate to "http://localhost:3000" on your browser
--- src/server.js
+++ src/server.js
@@ -5,9 +5,19 @@ const mysqlConnection = require("./connection");
 const filmsRoutes = require("./routes/films");
 
 var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('./public'));
+
 app.use(bodyParser.json());
 
 app.use("/films", filmsRoutes);
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+  });
 
 app.listen(3000);
 
